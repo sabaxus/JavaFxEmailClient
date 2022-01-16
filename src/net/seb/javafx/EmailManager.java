aa21fb9a7a4776ed8821a24026be08e78ec97fcb
sabaxus <sabaxro@yahoo.com>
2022-01-17 01:01:53 +0200

    Updating folder and the read/unread once an unread message was selected (read).

--- src/net/seb/javafx/EmailManager.java
+++ src/net/seb/javafx/EmailManager.java
@@ -4,13 +4,34 @@ import javafx.scene.control.TreeItem;
 import net.seb.javafx.controller.services.FetchFoldersService;
 import net.seb.javafx.controller.services.FolderUpdaterService;
 import net.seb.javafx.model.EmailAccount;
+import net.seb.javafx.model.EmailMessage;
 import net.seb.javafx.model.EmailTreeItem;
 
+import javax.mail.Flags;
 import javax.mail.Folder;
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmailManager {
+
+    private EmailMessage selectedMessage;
+    private EmailTreeItem<String> selectedFolder;
+
+    public EmailMessage getSelectedMessage() {
+        return selectedMessage;
+    }
+
+    public void setSelectedMessage(EmailMessage selectedMessage) {
+        this.selectedMessage = selectedMessage;
+    }
+
+    public EmailTreeItem<String> getSelectedFolder() {
+        return selectedFolder;
+    }
+
+    public void setSelectedFolder(EmailTreeItem<String> selectedFolder) {
+        this.selectedFolder = selectedFolder;
+    }
 
     private FolderUpdaterService folderUpdaterService;
 
@@ -37,5 +58,15 @@ public class EmailManager {
         FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem, folderList);
         fetchFoldersService.start();
         foldersRoot.getChildren().add(treeItem);
+    }
+
+    public void setRead() {
+        try {
+            selectedMessage.setRead(true);
+            selectedMessage.getMessage().setFlag(Flags.Flag.SEEN, true);
+            selectedFolder.decrementMessagesCount();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
