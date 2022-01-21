3c17cf95ffc8a55ccc70298e2869710530097637
sabaxus <sabaxro@yahoo.com>
2022-01-21 21:27:56 +0200

    Continue setting up the message details window.

--- src/net/seb/javafx/controller/EmailDetailsController.java
+++ src/net/seb/javafx/controller/EmailDetailsController.java
@@ -2,6 +2,7 @@ package net.seb.javafx.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.web.WebView;
@@ -10,10 +11,14 @@ import net.seb.javafx.controller.services.MessageRendererService;
 import net.seb.javafx.model.EmailMessage;
 import net.seb.javafx.view.ViewFactory;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeBodyPart;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class EmailDetailsController extends BaseController implements Initializable {
+
+    private String LOCATION_OF_DOWNLOADS = System.getProperty("user.home") + "\\Downloads\\";
 
     @FXML
     private Label subjectLabel;
@@ -40,8 +45,26 @@ public class EmailDetailsController extends BaseController implements Initializa
         subjectLabel.setText(emailMessage.getSubject());
         senderLabel.setText(emailMessage.getSender());
 
+        loadAttachments(emailMessage);
+
         MessageRendererService messageRendererService = new MessageRendererService(webView.getEngine());
         messageRendererService.setEmailMessage(emailMessage);
         messageRendererService.restart();
+    }
+
+    private void loadAttachments(EmailMessage emailMessage) {
+        if (emailMessage.hasAttachments()) {
+            for (MimeBodyPart mimeBodyPart : emailMessage.getAttachmentList()) {
+                Button button = null;
+                try {
+                    button = new Button(mimeBodyPart.getFileName());
+                    hBoxDownloads.getChildren().add(button);
+                } catch (MessagingException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            attachmentLabel.setText("");
+        }
     }
 }
--- src/net/seb/javafx/model/EmailMessage.java
+++ src/net/seb/javafx/model/EmailMessage.java
@@ -22,6 +22,14 @@ public class EmailMessage {
     private List<MimeBodyPart> attachmentList = new ArrayList<>();
     private boolean hasAttachments;
 
+    public boolean hasAttachments() {
+        return hasAttachments;
+    }
+
+    public List<MimeBodyPart> getAttachmentList() {
+        return attachmentList;
+    }
+
     public EmailMessage(String subject, String sender, String recipient,
                         int size, Date date, boolean isRead, Message message) {
         this.subject = new SimpleStringProperty(subject);
